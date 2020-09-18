cae079e53ca06dacdf5f6c55e61620c1b2b59cb4
Arthur Medeiros <arthur.silva@intellissis.com.br>
2020-09-17 22:15:32 -0300

    Working on new Interface

--- options.js
+++ options.js
@@ -1,6 +1,16 @@
 let mlist = document.getElementById('mlist');
 let saveBt = document.getElementById('save');
 
+let item = `
+    <div class="item">
+        <div class="mangaName">
+            <p>Se eu colocar uma titulo muito grande ele vai ficar assim</p>
+        </div>
+        <div class="mangaCap">
+            <p>Cap 80</p>
+        </div>
+    </div>`;
+
 function saveList(){
     var text = mlist.value;
     chrome.storage.sync.set({'list': text});
